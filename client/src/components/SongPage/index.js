7cd707ed54b3a945a5cd513f9ebf5107af322876
Jeremy Jones <jeremy.matthew.jones@gmail.com>
2020-08-29 11:03:55 -0500

    added search bar in SongPage Component
    Search bar in wrapped in div of className input-box to give search bar border
    search bar will be used for searching API for songs

--- client/src/components/SongPage/index.js
+++ client/src/components/SongPage/index.js
@@ -3,15 +3,25 @@ import React from "react";
 ////Components////
 import SongsCards from "../SongsCards";
 
-import {searchSongs} from "../utils/songs"
-
+import { searchSongs } from "../utils/songs";
 
 const SongPage = (props) => {
   return (
     <div className='container'>
       <div className='row'>
-          <h1>Song Page</h1>
-          <SongsCards songs={searchSongs}/>
+        <div className='col s10 offset-s1'>
+          <div className='input-box'>
+            <form>
+              <div className='input-field '>
+                <input id='search' type='search' required />
+                <button className="btn" type="submit" style={{ width: '100vh'}}>Find Songs</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+      <div className='row'>
+        <SongsCards songs={searchSongs} />
       </div>
     </div>
   );
