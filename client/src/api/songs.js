62114790a51077d3cde7c545c0fc25961d2852cf
Jeremy Jones <jeremy.matthew.jones@gmail.com>
2020-08-30 18:44:10 -0500

    changed fetchSongsApi to fetch from "/api/songs route"
    fetching from genius api can be done in the browser
    will implement when working on the searchPage

--- client/src/api/songs.js
+++ client/src/api/songs.js
@@ -1,9 +1,5 @@
 export function fetchSongsApi() {
-  return fetch("https://genius.p.rapidapi.com/songs/442856", {
+  return fetch("/api/songs", {
 	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "genius.p.rapidapi.com",
-		"x-rapidapi-key": "SIGN-UP-FOR-KEY"
-	}
-  }).then((res) => console.log(res));
+  }).then((res) => res.json());
 }
