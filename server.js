7e8eb6717c8b4108607e5e6eb1d5ac59287eff7b
Jeremy Jones <jeremy.matthew.jones@gmail.com>
2020-08-30 19:26:39 -0500

    predeployment setup

--- server.js
+++ server.js
@@ -1,15 +1,24 @@
 const express = require('express');
 const db = require('./config/database');
-
+const path = require('path');
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    app.get('*', function(req, res) {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    })
+}
+
 
 
 app.use('/api/songs/', require('./routes/api/songs'));
+
+
 
 const PORT = process.env.PORT || 5000;
 db();
