f1980fa05fa1812a032b5d3764c36cb555105cb9
Jeremy Jones <jeremy.matthew.jones@gmail.com>
2020-08-30 19:08:27 -0500

    removed card for upNextPage
    upNextPage not currently a container, commented out the rendering of SongsCard until implementation

--- client/src/components/UpNextPage/index.js
+++ client/src/components/UpNextPage/index.js
@@ -10,7 +10,7 @@ const UpNextPage = (props) => {
       <div className='row'>
         <div className='col s12 m6'>
           <h4>Up Next</h4>
-          <SongsCards songs={upNextSongs} />
+          {/* <SongsCards songs={upNextSongs} /> */}
         </div>
       </div>
     </div>
