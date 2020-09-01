cc4a613b92fe0ffdae0c2167b5c8c2cc82c3bc6f
Jeremy Jones <jeremy.matthew.jones@gmail.com>
2020-08-31 23:31:16 -0500

    removed local database connection from mongoose.connect()
    this is to test heroku deployment to fix Error: Cannot find module '../config/keys'

--- db/database.js
+++ db/database.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
-const db = require('../config/keys'). mongoURI;
+// const db = require('../config/keys'). mongoURI;
 
 
 
 function connectDatabase() {
 
-    mongoose.connect(process.env.MONGODB_URI || db,  
+    mongoose.connect(process.env.MONGODB_URI,  
     { useNewUrlParser: true, useUnifiedTopology: true})
         .then(() => {
             console.log('MongoDB Connected!')
