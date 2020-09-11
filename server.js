38e46f701b6205e2aaf4e0d27a7df18d4b1c9896
Jeremy Jones <jeremy.matthew.jones@gmail.com>
2020-09-10 21:36:24 -0500

    Fixed path to client build

--- server.js
+++ server.js
@@ -16,10 +16,8 @@ if (process.env.NODE_ENV === "production") {
 } 
 
 app.get('/', function(req, res) {
-   res.sendFile(path.join(__dirname, "../client/build/index.html"))
+   res.sendFile(path.join(__dirname, "./client/build/index.html"))
 })
-
-
 
 
 ////Routes////
