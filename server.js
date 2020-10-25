bd84b53f02162ee9edb806016284aef96f899e2a
Jeremy Jones <jeremy.matthew.jones@gmail.com>
2020-10-24 20:00:06 -0500

    move app.get route it production env check

--- server.js
+++ server.js
@@ -1,31 +1,27 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
-
-const routes = require('./routes')
+const routes = require("./routes");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-} 
 
-app.get('/', function(req, res) {
-   res.sendFile(path.join(__dirname, "./client/build/index.html"))
-})
-
+  app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  });
+}
 
 ////Routes////
 app.use(routes);
 
 // Database Connection
 
-
-
 app.listen(PORT, function serverConnected() {
-    console.log(`Karaoke Capital App listening on PORT ${PORT}`)
-})
+  console.log(`Karaoke Capital App listening on PORT ${PORT}`);
+});
