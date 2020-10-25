2f77af923b3c67fb04564ee2f3c97b9d4df7abf6
Jeremy Jones <jeremy.matthew.jones@gmail.com>
2020-10-24 20:42:33 -0500

    updating routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -5,8 +5,5 @@ const songRoutes = require('./songs');
 
 router.use('/songs', songRoutes);
 
-router.use(function(req, res) {
-    res.sendFile(path.join(__dirname, "../client/build/index.html"))
-})
 
 module.exports = router;
