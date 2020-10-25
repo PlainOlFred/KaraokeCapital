dfaa5923a4eb37644137ec8e72b39f886c4da9b9
Jeremy Jones <jeremy.matthew.jones@gmail.com>
2020-10-24 20:32:20 -0500

    updating proxy

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require("path");
 const routes = require("./routes");
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
