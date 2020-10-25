6ed9f6346f8e5729413cfdc6cca94a485a4ab824
Jeremy Jones <jeremy.matthew.jones@gmail.com>
2020-10-24 19:44:46 -0500

    deployment

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ if (process.env.NODE_ENV === "production") {
 } 
 
 app.get('/', function(req, res) {
-   res.sendFile(path.join(__dirname, "./client/public/index.html"))
+   res.sendFile(path.join(__dirname, "./client/build/index.html"))
 })
 
 
